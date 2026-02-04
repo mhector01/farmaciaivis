9231f9c68f1a3a853f2e823d495cd871028720ee
Héctor Mendoza <hmendoza@iplideres.com>
2026-02-03 22:40:36 -0600

    modified conexion

--- model/Conexion.php
+++ model/Conexion.php
@@ -10,10 +10,10 @@
 		    
 		   
 			$driver = 'mysql'; //mysql no cambiar
-			$host = 'q00w40ssg40k04s84gkokkkk'; //localhost
-			$dbname = 'farmaciaivis'; //bdd
+			$host = 'f8skkow4o0cwgwkc4gkokwko'; //localhost
+			$dbname = 'farmaciasistem'; //bdd
 			$username ='root'; //usuario
-			$passwd = 'Rangermiperro2.'; //contra
+			$passwd = 'L2LeZYkXaLZSwo83xph0B1W0i4KERnWlBUjQmgqfO6ddseD6wpxq6ggA73LigRsp.'; //contra
 
 
 
